da8c370c36193d1545852975a99fbf06bb87a4fc
luciaasen <luciaasen@gmail.com>
2017-04-30 01:05:08 +0200

    Quiero creer que funcionan los dos primeros apartados

--- p5/src/p5/DefaultObservableProperty.java
+++ p5/src/p5/DefaultObservableProperty.java
@@ -13,8 +13,8 @@ import p5.interf.PropertyObserver;
  *
  */
 public abstract class DefaultObservableProperty<V>  implements ObservableProperty<V>{
-	Optional<V> value;
-	Optional<ArrayList<PropertyObserver<V>>> observers;
+	protected Optional<V> value;
+	protected Optional<ArrayList<PropertyObserver<V>>> observers;
 	
 	/**
 	 * Creates a DedaultObservableProperty with value
@@ -33,13 +33,38 @@ public abstract class DefaultObservableProperty<V>  implements ObservablePropert
 	public V getValue(){
 		return this.value.orElse(null);
 	}
+	
+	/**
+	 * Adds the observer from de property observers list
+	 * @param o the PropertyObserver to remove
+	 */
 	public void addObserver( PropertyObserver<V> o){
 		this.observers.orElse(null).add(o);
 	}
+	
+	/**
+	 * Removes the observer from de property observers list
+	 * @param o the PropertyObserver to remove
+	 */
 	public void removeObserver(PropertyObserver<V> o){
 		this.observers.orElse(null).remove(o);
 	}
+	
+	/**
+	 * if the value is present, updates it and, if changed, notifies all the observers
+	 * @param newValue value to set
+	 */
 	protected void setValue(V newValue){
-		
+		if(this.value.isPresent()){
+			if(this.value.get() != newValue){
+				V oldValue = this.value.get();
+				this.value = Optional.of(newValue);
+				/*En el else del orElse devolvemos una lista vacia en vez de null para evitar una excepcion
+				 * Al estar la lista vacia, no entrara en el bucle*/
+				for (PropertyObserver<V> obs: this.observers.orElse(new ArrayList<PropertyObserver<V>>())){
+					obs.propertyChanged(this, oldValue);
+				}
+			}
+		}
 	}	
 }
--- p5/src/p5/exc/IllegalArgumentException.java
+++ p5/src/p5/exc/IllegalArgumentException.java
@@ -3,10 +3,17 @@
  */
 package p5.exc;
 
+import p5.Task;
+
 /**
  * @author lucia
  *
  */
 public class IllegalArgumentException extends RuntimeException{
+	
+	
+	public IllegalArgumentException(){
+		System.out.println("No es posible manejar el argumento ");
+	}
 
 }
