07c814ea8a6ce5f627d8450225e61ddb9dad84b6
luciaasen <luciaasen@gmail.com>
2017-04-30 01:10:56 +0200

    Quiero creer que funcionan los dos primeros apartados

--- p5/src/p5/MyAdjustableTime.java
+++ p5/src/p5/MyAdjustableTime.java
@@ -10,6 +10,10 @@ public class MyAdjustableTime extends DefaultObservableProperty<Integer> impleme
 	}
 	
 	@Override
+	/**
+	 * If the incremented value is not smaller than 0, increments value and notify observers
+	 * @param inc the increment of the value
+	 */
 	public void incrementTime(int inc) throws IllegalArgumentException {
 		Integer oldValue = super.value.orElse(0);
 		Integer	newValue = oldValue + inc;
@@ -20,6 +24,11 @@ public class MyAdjustableTime extends DefaultObservableProperty<Integer> impleme
 	}
 
 	@Override
+	/**
+	 * The observer behaviour to a change is incrementing its value with the same inc as its observable property
+	 * @param property the property that has changed
+	 * @param oldValue 
+	 */
 	public void propertyChanged(ObservableProperty<Integer> property, Integer oldValue) {
 		Integer inc = property.getValue() - oldValue;
 		this.incrementTime(inc);
